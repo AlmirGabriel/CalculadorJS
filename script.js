6a9e3384d82db8160d2fa5ed01442925fb3b7e8d
Almir Gabriel <almir.gabriel.andrade@gmail.com>
2023-08-20 20:25:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function calculadora () {
+    const operacao = Number(prompt('Escolha a operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - Divisão Real (/)\n 5 - Divisão Inteira (%)\n 6 - Potenciação (**)'))
+}
+
+if (!operacao || operacao >= 7) {
+    alert('Erro , operação invalida!!!')
+    calculadora();
+    else {
+        let n1 = Number(prompt('Insira o primeiro numero.'));
+        let n2 = Number(prompt('Insira o segundo numero.'))
+
+        
+    }
+}
